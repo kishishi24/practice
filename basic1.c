64401a6b1cef2a8bdf974b0844b1e6f6f27238e7
kishishi24 <kishi-5@i.softbank.jp>
2021-01-03 17:36:31 +0900

    practice1/2021/1/3

---
+++ basic1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ 
+int main() {
+ 
+  printf("Hello, World\n");//printf
+  char var;//変数の宣言
+  var='A';
+  int a,b,c;
+  a=0;b=1;c=2;
+  printf("%d %d %dです\n",a,a+b,c);
+  float x;
+  x=33.33;
+  printf("\a%f\n",x);
+  return 0;
+ 
+}
