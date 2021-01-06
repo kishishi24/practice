d883265c0f54eb4cb553718caea1dbf98225d11d
kishishi24 <kishi-5@i.softbank.jp>
2021-01-06 12:12:13 +0900

    実数範囲の解探索はほぼ達成

--- basic2.c
+++ basic2.c
@@ -2,6 +2,7 @@
 #include<string.h>//文字列をいじる関数を使うのに必要
 #include<stdlib.h>//rand乱数関数を使うのに必要
 #include<math.h>//pow（累乗)を使うのに必要
+#include<time.h>//timeを使う
 
 #define DELTA 1.0e-9//数値微分に使うデルタ
 typedef struct {//複素数を表現する構造体の定義
@@ -10,6 +11,8 @@ typedef struct {//複素数を表現する構造体の定義
 } complex;
 int di;//di:方程式の次数
 double coe[100];//coe[]:係数を入れる配列
+double sol[100];//解を入れる配列(実数)
+complex sol_c[100];//解を入れる配列(複素数)
 double Fx(double);//入力の値(実数）に対する関数値を返す
 complex conv_c(double);//実数を複素数で扱うための関数
 complex add_c(complex,complex);//複素数の和を返す
@@ -19,6 +22,7 @@ complex pow_c(complex,int);//複素数の累乗を返す
 double val_c(complex);//複素数の大きさを返す
 void show_c(complex);//複素数を表示する
 complex Fx_c(complex);//入力の値(複素数)に対する関数値を返す
+int comp_c(complex,complex);//複素数が一致するかの判定
 complex dif_c(complex (*Fx_c)(complex),complex);//関数の微分を返す
 //void newtonMethod(double a,double e,double *result,double *num);//ニュートンラフソン（実数)
 complex newtonMethod_c(complex a,double e);//  ニュートンラフソン(複素数)
@@ -67,7 +71,7 @@ complex pow_c(complex a,int n){
     return (b);
 }
 void show_c(complex a){
-    printf("%lf+i%lf",a.real,a.imag);
+    printf("%.3f+i%.3f\n",a.real,a.imag);
 }
 double val_c(complex a){
     return(sqrt(pow(a.real,2)+pow(a.imag,2)));
@@ -96,6 +100,13 @@ complex dif_c(complex (*Fx_c)(complex),complex xd){
     DIF = div_c(deno,conv_c(2*DELTA));
     return DIF;
 }
+int comp_c(complex a,complex b){
+    if(fabs(a.real-b.real)<0.01&&fabs(a.imag-b.imag)<0.01){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 complex newtonMethod_c(complex a,double e){
     int i=0;
     complex aa;
@@ -119,15 +130,34 @@ int main(){
         scanf("%lf",&coe[i]);
         //printf("%lf\n",coe[i]);//確認
     }
-    printf("数式は     \n");
+    printf("対象とする方程式は     \n");
     for(int j=0;j<=di;j++){
         get_sign(coe[j]);
         printf("%fx^%d",coe[j],di-j);
     };//(数式表示用)
-    printf("\n");
+    printf("=0\n");
     //printf("%lf",Fx(2.0));
-    printf("答えは、");
-    show_c(newtonMethod_c(conv_c(2),DELTA));
+    printf("答えは、\n");
+    unsigned int seed;
+    seed = (unsigned int)time(NULL);
+    srand(seed);
+    int r=1;
+    sol_c[0]=newtonMethod_c(conv_c(0),DELTA);
+    for(int i=0;i<1000;i++){
+        double x=(rand()%10001)-5000;
+        sol_c[r]=newtonMethod_c(conv_c(x),DELTA);
+        for(int j=0;j<r;j++){
+            if(comp_c(sol_c[r],sol_c[j])==1){
+                break;
+            }if(j==r-1){
+                r++;break;
+            }
+        }
+    }
+    for(int i=0;i<r;i++){
+        printf("%d:",i);
+        show_c(sol_c[i]);
+    }
     return 0;
 }
 
